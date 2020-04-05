81d0c56fcd5e8feb783b323ccf4415573faa682f
hejin <hejin82@gmail.com>
2020-04-06 04:40:07 +0900

    ng generate @angular/material:dashboard app-dashborad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,19 @@ import { FormComponent } from './core/form/form.component';
 import { MessageModule } from './messages/message.module';
 import { MessageComponent } from './messages/message.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppDashboradComponent } from './app-dashborad/app-dashborad.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    AppDashboradComponent,
     // TableComponent,
     // FormComponent,
     // MessageComponent
@@ -24,7 +32,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ModelModule,
     CoreModule,
     MessageModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [
