9c94d9c790f15e20d0467e2e9ffb4209f7e6050d
hejin <hejin82@gmail.com>
2020-05-15 07:32:17 +0900

    delete all

--- src/app/model/model.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { StaticDatasource } from './static.datasource';
-import { Model } from './repository.model';
-
-@NgModule({
-  providers: [Model, StaticDatasource]
-})
-export class ModelModule { }
