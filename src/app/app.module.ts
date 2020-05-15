67670e7d9172adc99c2f74e1b6d9d05ca4e2f829
hejin <jin-he@nec.com>
2020-05-15 09:39:50 +0900

    ng generate component reddit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { RedditComponent } from './reddit/reddit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     UserItemComponent,
-    UserListComponent
+    UserListComponent,
+    RedditComponent
   ],
   imports: [
     BrowserModule
