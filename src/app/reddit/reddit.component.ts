cf10629c13d1e6be86ae7a90c5f8e219857cce05
hejin <jin-he@nec.com>
2020-05-15 10:55:05 +0900

    commit

--- src/app/reddit/reddit.component.ts
+++ src/app/reddit/reddit.component.ts
@@ -23,6 +23,9 @@ export class RedditComponent implements OnInit {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title:${title.value} and link:${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 
